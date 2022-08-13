be8c7b390d8a9808081cf3b5e1b76b7a09b062db
CHandrakant <chandrakantgaikwad6639@gmail.com>
2022-08-13 15:13:06 +0530

    tes5

--- backend/moongoseapi.ts
+++ backend/moongoseapi.ts
@@ -7,6 +7,9 @@ let cors = require('cors')
 const product=require('./routes/Productroute')
 const app=express()
 const error=require('./middleware/errorHandle')
+app.use(express.static('public'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors())
 app.use(express.json())
 app.use(cookieparser())
