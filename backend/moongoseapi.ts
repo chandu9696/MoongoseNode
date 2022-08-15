0c79064e743fd1ef6944165c4c9d95436b7da829
CHandrakant <chandrakantgaikwad6639@gmail.com>
2022-08-15 20:38:28 +0530

    headerset

--- backend/moongoseapi.ts
+++ backend/moongoseapi.ts
@@ -10,7 +10,7 @@ const error=require('./middleware/errorHandle')
 app.use(express.static('public'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors())
+app.use(cors(cors({credentials: true, origin: 'http://localhost:3000'})))
 app.use(express.json())
 app.use(cookieparser())
 const user=require('./routes/UserRoutes')
