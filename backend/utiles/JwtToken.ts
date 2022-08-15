bcb1499249315605cd9b05af1896f16d3e2ebb14
CHandrakant <chandrakantgaikwad6639@gmail.com>
2022-08-15 19:47:23 +0530

    headerset

--- backend/utiles/JwtToken.ts
+++ backend/utiles/JwtToken.ts
@@ -1,19 +1,22 @@
 const sendToken = (user:any, statusCode:any, res:any) => {
     const token = user.getJWTToken();
-  
+
     // options for cookie
     const options = {
       expires: new Date(
         Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      // headers:{
+      //   'Access-Control-Allow-Credentials': true
+      // }
     };
-  
-    res.status(statusCode).cookie("token", token, options).json({
+
+    res.status(statusCode).header({'Access-Control-Allow-Origin':true}).cookie("token", token, options).json({
       success: true,
       user,
       token,
     });
   };
-  
+
   module.exports = sendToken;
